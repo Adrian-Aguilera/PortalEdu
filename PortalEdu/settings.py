d1e23433ea012c242caf9a5f17c0a88780655f81
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-04-29 11:11:11 -0600

    feat: configuracion archivos estaticos

--- PortalEdu/settings.py
+++ PortalEdu/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'django_project/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'PortalEdu/static')
+
+#archivos estaticos por app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'login/static')
+]
 
 
 # Default primary key field type
